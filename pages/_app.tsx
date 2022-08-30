2674dd455703b6c0c8ac459a61bbfd01cc6e3293
rabobahago <rabobahago@gmail.com>
2022-08-30 08:30:03 +0100

    api, detail, search and upload components added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,40 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { useState, useEffect } from "react";
+import "../styles/globals.css";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import type { AppProps } from "next/app";
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  //next can both run on server and client side
+  //at a start this page will be serverside render, and that is why we are setting isSSR to true in this component
+  const [isSSR, setisSSR] = useState(true);
+  //at the start this component is serverside but we have tell where it will change to client side render
+  //that is why setisSSR is set to force when ever the component run once, this achieve with useEffect that run once with
+  //empty array.
 
-export default MyApp
+  useEffect(() => {
+    setisSSR(false);
+  }, []);
+  //if serverside render stop from running client side, that is stop running client side rendering
+  if (isSSR) return null;
+  return (
+    <GoogleOAuthProvider
+      clientId={`${process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN}`}
+    >
+      <div className="xl:w-[1200px] m-auto overflow-hidden h-[100vh]">
+        <Navbar />
+        <div className="flex gap-6 md:gap-20">
+          <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
+            <Sidebar />
+          </div>
+          <div className="mt-4 flex flex-col gap-10 overflow-auto h-[88vh] video flex-1">
+            <Component {...pageProps} />
+          </div>
+        </div>
+      </div>
+    </GoogleOAuthProvider>
+  );
+};
+export default MyApp;
+//https://tiktok-clone-final-izkp5le3d-rabobahago.vercel.app/
